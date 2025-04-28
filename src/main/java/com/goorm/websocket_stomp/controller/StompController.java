9367509858010d918ff7a801051d47232e1b3180
ahn_h_j <tkgkd246@gmail.com>
2025-04-28 14:02:59 +0900

    feat : SendToUser 방식

--- src/main/java/com/goorm/websocket_stomp/controller/StompController.java
+++ src/main/java/com/goorm/websocket_stomp/controller/StompController.java
@@ -2,19 +2,28 @@ package com.goorm.websocket_stomp.controller;
 
 import com.goorm.websocket_stomp.dto.ReqDto;
 import com.goorm.websocket_stomp.dto.ResDto;
+import com.goorm.websocket_stomp.dto.ResSessionsDto;
+import com.goorm.websocket_stomp.listener.StompEventListener;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
 import java.time.LocalDateTime;
+import java.util.EventListener;
+import java.util.Set;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class StompController {
+
+    private final StompEventListener stompEventListener;
+
     //여러 값을 받을 수 있음
     @MessageMapping("/hello")//수신 == /app/hello
     @SendTo("/topic/hello")//발신
@@ -32,5 +41,20 @@ public class StompController {
         log.info("reqDto : {}", reqDto);
 
         return new ResDto(reqDto.getMessage().toUpperCase(), LocalDateTime.now());
+    }
+
+    @MessageMapping("/sessions")//수신 == /app/sessions
+    @SendToUser("/queue/sessions")// == /user/queue/sessions
+    //특정 세션에만 보내는 것
+    // queue : 1:1 통신에 많이 사용하는 prefix
+    public ResSessionsDto sessions(ReqDto reqDto, MessageHeaders headers){
+
+        log.info("reqDto : {}", reqDto);
+        String sessionId = headers.get("simpSessionId").toString();
+        log.info("sessionId : {}", sessionId);
+
+        Set<String> sessions = stompEventListener.getSessions();
+
+        return new ResSessionsDto(sessions.size(), sessions.stream().toList(), sessionId, LocalDateTime.now());
     }
 }
